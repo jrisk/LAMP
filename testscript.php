0f7338aac748aa47b4e49ae9c535240d3c733b37
jrisk <joeyrsk@gmail.com>
2016-01-02 00:40:25 -0600

    object parse testing

---
+++ testscript.php
@@ -0,0 +1,11 @@
+<?php
+
+phpinfo();
+
+$w = stream_get_wrappers();
+echo 'openssl: ',  extension_loaded  ('openssl') ? 'yes':'no', "\n";
+echo 'http wrapper: ', in_array('http', $w) ? 'yes':'no', "\n";
+echo 'https wrapper: ', in_array('https', $w) ? 'yes':'no', "\n";
+echo 'wrappers: ', var_dump($w);
+
+?>
